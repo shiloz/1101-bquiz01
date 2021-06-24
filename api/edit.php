e330c2d834b1ce314523fde96628fb89a6c9b7ad
mackliu <macklun@gmail.com>
2021-06-24 15:09:08 +0800

    完成網頁標題增改刪查功能

--- api/edit.php
+++ api/edit.php
@@ -4,17 +4,24 @@ $texts=$_POST['text'];
 $ids=$_POST['id'];
 
 foreach($ids as $key => $id){
-    $row=$Title->find($id);
-    $row['text']=$texts[$key];
 
-    if(isset($_POST['sh']) && $_POST['sh']==$id){
-        $row['sh']=1;
+    if(isset($_POST['del']) && in_array($id,$_POST['del'])){
+        $Title->del($id);
     }else{
-        $row['sh']=0;
+
+        $row=$Title->find($id);
+        $row['text']=$texts[$key];
+        $row['sh']=(isset($_POST['sh']) && $_POST['sh']==$id)?1:0;
+
+/*         if(isset($_POST['sh']) && $_POST['sh']==$id){
+            $row['sh']=1;
+        }else{
+            $row['sh']=0;
+        } */
+    
+        $Title->save($row);
+
     }
-
-    $Title->save($row);
 }
-
 
 to("../backend.php?do=title");
