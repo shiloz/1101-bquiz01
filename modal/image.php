a222917f98ee5e04f2c42bd12f650da8c32b39fd
mackliu <macklun@gmail.com>
2021-06-24 10:58:00 +0800

    完成後台各頁面及彈出視窗html碼撰寫

--- modal/image.php
+++ modal/image.php
@@ -1,17 +1,13 @@
 <?php include_once "../base.php";?>
 
-<h3 class='cent'><?=$as['title'];?></h3>
+<h3 class='cent'><?=$as['image'];?></h3>
 <hr>
 
 <form action="api/add.php" method="post" enctype="multipart/form-data">
 <table style="margin:auto;">
     <tr>
-        <td style="text-align:right"><?=$hs['title'];?>：</td>
+        <td style="text-align:right"><?=$hs['image'];?>：</td>
         <td style="text-align:right"><input type="file" name="img"></td>
-    </tr>
-    <tr>
-        <td>標題區替代文字：</td>
-        <td><input type="text" name="text"></td>
     </tr>
 </table>
 <div class="cent">
--- modal/menu.php
+++ modal/menu.php
@@ -1,17 +1,17 @@
 <?php include_once "../base.php";?>
 
-<h3 class='cent'><?=$as['title'];?></h3>
+<h3 class='cent'><?=$as['menu'];?></h3>
 <hr>
 
 <form action="api/add.php" method="post" enctype="multipart/form-data">
 <table style="margin:auto;">
     <tr>
-        <td style="text-align:right"><?=$hs['title'];?>：</td>
-        <td style="text-align:right"><input type="file" name="img"></td>
+        <td style="text-align:right">主選單名稱：</td>
+        <td style="text-align:right"><input type="text" name="text"></td>
     </tr>
     <tr>
-        <td>標題區替代文字：</td>
-        <td><input type="text" name="text"></td>
+        <td>主選單連結網址：</td>
+        <td><input type="text" name="href"></td>
     </tr>
 </table>
 <div class="cent">
