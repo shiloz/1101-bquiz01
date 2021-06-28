eb751ffa77d78a10f5456cbeaaf5c324b7bf1403
mackliu <macklun@gmail.com>
2021-06-28 11:58:02 +0800

    完成前端校園映像圖片顯示

--- index.php
+++ index.php
@@ -50,16 +50,41 @@
                 	<button style="width:100%; margin-left:auto; margin-right:auto; margin-top:2px; height:50px;" onclick="lo(&#39;?do=login&#39;)">管理登入</button>
                 	<div style="width:89%; height:480px;" class="dbor">
                     	<span class="t botli">校園映象區</span>
-						                        <script>
-                        	var nowpage=0,num=0;
+						<div class="cent" onclick='pp(1)'>
+							<img src="icon/up.jpg">
+						</div>
+						<?php
+						$imgs=$Image->all(['sh'=>1]);
+						foreach($imgs as $key => $img){
+							echo "<div class='cent im' id='ssaa$key'>";
+							echo "<img src='img/{$img['img']}' style='width:150px;height:103px;margin:2px;border:3px solid orange'>";
+
+							echo "</div>";
+						}
+
+
+						?>
+
+						<div class="cent" onclick='pp(2)'>
+						<img src="icon/dn.jpg">
+						</div>
+
+
+
+
+
+						<script>
+                        	var nowpage=0,num=<?=$Image->count(['sh'=>1]);?>;
 							function pp(x)
 							{
 								var s,t;
 								if(x==1&&nowpage-1>=0)
 								{nowpage--;}
-								if(x==2&&(nowpage+1)*3<=num*1+3)
+
+								if(x==2&&(nowpage+1)<=(num-3))
 								{nowpage++;}
 								$(".im").hide()
+
 								for(s=0;s<=2;s++)
 								{
 									t=s*1+nowpage*1;
