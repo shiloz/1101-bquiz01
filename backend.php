5a1884f86e56a72d57d7d03611bdbc301f94084f
mackliu <macklun@gmail.com>
2021-06-28 08:48:38 +0800

    在前端顯示網站標題圖片

--- backend.php
+++ backend.php
@@ -35,10 +35,11 @@
     </div>
 
     <div id="main">
-        <a title="" href="?">
-            <div class="ti" style="background:url(&#39;use/&#39;); background-size:cover;"></div>
-            <!--標題-->
-        </a>
+    <a title="<?=$Title->find(['sh'=>1])['text'];?>" href="index.php" alt="">
+        <div class="ti" style="background:url(&#39;img/<?=$Title->find(['sh'=>1])['img'];?>&#39;); background-size:cover;">
+        </div>
+    <!--標題-->
+    </a>
         <div id="ms">
             <div id="lf" style="float:left;">
                 <div id="menuput" class="dbor">
